6dddee874f566adbb5ef2fdcd376307f5caec94a
feddersmichael <fedders.michael@gmail.com>
2023-11-23 00:51:53 +0100

    step

--- R/exact_sppDS.R
+++
@@ -1,4 +0,0 @@
-
-exact_sppDS <- function(){
-  
-}
--- R/exact_spp_candDS.R
+++ R/exact_spp_candDS.R
@@ -1,6 +1,13 @@
 
-
 # data = name of data, string
-exact_spp_candDS <- function(data){
+exact_spp_candDS <- function(data_name){
+  
+  # We convert the string into an object
+  training_data <- eval(parse(text = data_name), envir = parent.frame())
+  
+  # should probably just be saved from the start
+  column_names <- names(training_data)
+  
+  
   
 }
