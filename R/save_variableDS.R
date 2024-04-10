10fa0ef0d52792dec8f4dc6229c747235581492b
feddersmichael <fedders.michael@gmail.com>
2024-04-10 18:52:06 +0200

    variable_save

--- R/save_variableDS.R
+++ R/save_variableDS.R
@@ -2,14 +2,18 @@
 #' Save a variable
 #'
 #' @param data_name Name of the data.
+#' @param variable_name Name of the variable which we want to save.
 #' @param variable The variable which shall be saved.
 #'
 #' @return The variable which shall be saved.
 #' @export
-save_variableDS <- function(data_name, variable) {
+save_variableDS <- function(data_name, variable_name, variable) {
   
-  if (exists(paste0(data_name, "_", variable), envir = parent.frame())) {
-    stop("The variable name '", data_name, "_", variable, "'is already in use.")
+  if (!is.null(variable_name) &&
+      exists(paste0(data_name, "_", variable_name), envir = parent.frame())) {
+    stop("The variable name '", data_name, "_", variable_name,
+         "'is already in use.")
   }
+  
   return(variable)
 }
